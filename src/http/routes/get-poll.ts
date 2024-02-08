2e2b68860474910b6c86ff966d0cf82363937c9b
Renan <renan.fachin@hotmail.com>
2024-02-08 13:46:16 -0300

    implements the return of the number of votes in each poll option in the get route

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import { z } from "zod"
 import { prisma } from "../../lib/prisma"
 import { FastifyInstance } from "fastify"
+import { redis } from "../../lib/redis"
 
 export async function getPoll(app: FastifyInstance) {
   app.get('/polls/:pollId', async (request, reply) => {
@@ -28,9 +29,63 @@ export async function getPoll(app: FastifyInstance) {
       }
     })
 
+    if (!poll) {
+      return reply.status(400).send({ message: 'Poll not found.' })
+    }
+
+    // buscando as informações do redis sobre a enquete
+    const result = await redis.zrange(pollId, 0, -1, 'WITHSCORES') // 0, -1 -> TODAS OPÇÕES
+    /*
+    retorno do redis 
+     - índice par = id da opção
+     - índice impar = quantidade de votos na opção
+    [
+      'c5657e9f-7ec3-420d-b0c3-55aeec388673',
+      '0',
+      '9d473c3f-b459-478c-ac05-9aa6d93cf7a5',
+      '1'
+    ]
+    */
+
+    // Ajustando este array para uma nova estrutura
+    // Record<tipo da chave, tipo do valor da chave> -> é objeto para o typescript
+    const votes = result.reduce((initialObj, resultLine, index) => {
+      // Toda vez que o índice encontrado for PAR -> armazenar na variável score a próxima informação do array (que é a quantidade de votos referente a esta opção)
+      if (index % 2 === 0) {
+        const score = result[index + 1]
+
+        // Adicionando no array inicial as informações de id:score
+        Object.assign(initialObj, { [resultLine]: Number(score) })
+      }
+
+
+      return initialObj
+    }, {} as Record<string, number>)
+
+    /* 
+      agora o retorno é:
+      {
+        'c5657e9f-7ec3-420d-b0c3-55aeec388673': '0',
+        '9d473c3f-b459-478c-ac05-9aa6d93cf7a5': '1'
+      }
+    */
+
+
+
     // Retornando o objeto por completo
     return reply.status(200).send({
-      poll
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map(option => {
+          return {
+            id: option.id,
+            title: option.title,
+            // Caso a opção tenha sido votada e exista dentro do retorno do redis, mostrar a quantidade referente, caso contrário, mostrar 0
+            score: (option.id in votes) ? votes[option.id] : 0
+          }
+        })
+      }
     })
   })
 }
